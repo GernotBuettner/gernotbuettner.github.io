c492af286890d484011fff1f82ca0c6e80bc2cdf
gbt <gernot_buettner@yahoo.de>
2024-05-06 16:38:01 +0200

    Install webpack

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,8 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/assets/fonts/pp-neue-machina-plain-regular.otf");
 	eleventyConfig.addPassthroughCopy("src/assets/fonts/pp-neue-machina-plain-ultra-bold.otf");
 
+	// eleventyConfig.addPassthroughCopy("src/assets/js/main.js");
+
 	return {
 		dir: {
 			input: "src",
