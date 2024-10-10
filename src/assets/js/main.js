5a5ba0bc5de1e9acb25015fa67a3e578aa0df1ce
gbt <gernot_buettner@yahoo.de>
2024-10-10 11:13:37 +0200

    Skill section and animation

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,8 @@
 import createComponentFactory from './library/create-component-factory';
 import AnimatedBackgroundComponent from './components/animated-background';
 import CustomCursorComponent from './components/custom-cursor';
+import MovingGradientComponent from './components/moving-gradient';
 
 createComponentFactory(AnimatedBackgroundComponent, 'animated-background');
 createComponentFactory(CustomCursorComponent, 'custom-cursor');
+createComponentFactory(MovingGradientComponent, 'moving-gradient');
