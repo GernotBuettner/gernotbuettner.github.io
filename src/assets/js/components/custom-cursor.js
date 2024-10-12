efa9e2e9e763f75423841b484ed48cf50ec07e42
gbt <gernot_buettner@yahoo.de>
2024-10-12 14:12:41 +0200

    Add custom cursor animation

--- src/assets/js/components/custom-cursor.js
+++ src/assets/js/components/custom-cursor.js
@@ -2,6 +2,7 @@
 export default class CustomCursorCompontent {
 	constructor(element) {
 		this.element = element;
+		this.linkModifierClass = 'custom-cursor--link-hovered'
 	}
 
 	init() {
@@ -10,8 +11,30 @@ export default class CustomCursorCompontent {
 
 	initEventListeners() {
 		document.addEventListener('mousemove', this.onMouseMove.bind(this));
+		document.addEventListener('mouseover', this.onMouseOver.bind(this));
 		window.addEventListener('contextmenu', this.onMouseOutOfBounds.bind(this));
 		document.addEventListener('mouseleave', this.onMouseOutOfBounds.bind(this));
+	}
+
+	onMouseOver(event) {
+		const closestLink = event.target.closest('a');
+		const closestButton = event.target.closest('button');
+		const elementClassList = this.element.classList;
+
+		if (closestLink || closestButton) {
+			/*
+			TODO: Add check on Scroll as well
+			Make sure to remove class on scroll only if the cursor is not hovering the animated area
+			Area is a bit losely defined. Probably should remove when the cursor position + radius of
+			the gradient is not above the skill box.
+			*/
+
+		if (!elementClassList.contains(this.linkModifierClass)) {
+				elementClassList.add(this.linkModifierClass);
+			}
+		} else {
+			elementClassList.remove(this.linkModifierClass);
+		}
 	}
 
 	onMouseMove(event) {
