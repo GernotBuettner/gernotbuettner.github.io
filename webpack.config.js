1c7e5c9f830f722ed1f335d50f5490451e9cccd0
GernotBuettner <GernotBuettner@users.noreply.github.com>
2024-12-05 17:37:59 +0000

    deploy: 035ac092cee8a7ccf83d8677f2ab7cbd6c2c470e

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-	entry: './src/assets/js/main.js',
-	output: {
-		path: path.resolve(__dirname, 'dist/assets/js'),
-		filename: 'main.js'
-	}
-};
